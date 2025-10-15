297fe3a71be9774f867115320ecea138377e66d0
Jesse Harlan <jesse.harlan@centralia.edu>
2025-10-15 11:25:55 -0700

    added template

---
+++ CCMovieDatabase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CCMovieDatabase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
