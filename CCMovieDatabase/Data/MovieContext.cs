e08bd3e3a4f242b06ef80c3a9e85bf8c72f7b97a
cody norwood <cody.norwood@student.centralia.edu>
2025-10-20 11:30:51 -0700

    Added Movie controller

---
+++ CCMovieDatabase/Data/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CCMovieDatabase.Models;
+
+namespace CCMovieDatabase.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CCMovieDatabase.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- CCMovieDatabase/Program.cs
+++ CCMovieDatabase/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CCMovieDatabase.Data;
 namespace CCMovieDatabase
 {
     public class Program
@@ -5,6 +8,8 @@ namespace CCMovieDatabase
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext") ?? throw new InvalidOperationException("Connection string 'MovieContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
