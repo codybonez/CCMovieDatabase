fed37cd394da941534ff542b95040b1102ca31e5
Jesse Harlan <jesse.harlan@centralia.edu>
2025-11-03 11:22:38 -0800

    Feat: added article, character, crewcredit, and person tables

---
+++ CCMovieDatabase/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace CCMovieDatabase.Models
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Slug { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ CCMovieDatabase/Models/Character.cs
@@ -0,0 +1,9 @@
+﻿namespace CCMovieDatabase.Models
+{
+    public class Character
+    {
+        public int CharacterId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
